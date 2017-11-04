a5bdaccfe71e0dd9cc58d5db1cd56e66d75cd47c
Daniel Hornberger <hor12020@byui.edu>
2017-11-04 15:15:23 -0600

    PopulateListTask now creates a list of Game objects that can be referenced

--- app/src/main/java/com/pickup/daniel/pick_up/PopulateListTask.java
+++ app/src/main/java/com/pickup/daniel/pick_up/PopulateListTask.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -18,6 +19,24 @@ import java.util.Random;
  */
 
 public class PopulateListTask extends AsyncTask<Void, String, Void> {
+
+    // 13 Game types
+    String games[] = {"Basketball", "Badminton", "Football", "Golf", "Hockey",
+            "Raquetball", "Rugby", "Softball", "Soccer", "Spike Ball", "Ultimate Frisbee",
+            "Volley Ball", "Walley Ball"};
+
+    String days[] = {"Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
+
+    // 16 players
+    String players[] = {"Joe", "Sally", "John", "Caleb", "Kevin", "Corey", "Robert", "Ryan",
+            "Vanessa", "Connor", "Tucker", "Annie", "Karley", "Kendra", "Samantha", "Kayla"};
+
+    // Four Outdoor locations
+    String outdoorLocations[] = {"Upper Playing Fields", "Football field",
+            "Porter Park", "Lower Playing Fields"};
+
+    // Two indoor locations
+    String indoorLocations[] = {"I-Center Courts", "Hart Gym"};
 
     Context _currentContext;
     // We need to pass the Array Adapter here so we can use it
@@ -37,39 +56,30 @@ public class PopulateListTask extends AsyncTask<Void, String, Void> {
         super.onPreExecute();
     }
 
-    
+
 
     // Separate thread. We can't update the list from this method because it would access the GUI
     @Override
     protected Void doInBackground(Void... params) {
+        // Generate 35 random game configurations
         int numGames = 35;
 
         String listItem = "";
         String game;
-        String games[] = {"Basketball", "Badminton", "Football", "Golf", "Hockey",
-                "Raquetball", "Rugby", "Softball", "Soccer", "Spike Ball", "Ultimate Frisbee",
-                "Volley Ball", "Walley Ball"};
+
         int hour = 0;
         String day;
-        String days[] = {"Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
-
-        String players[] = {"Joe", "Sally", "John", "Caleb", "Kevin", "Corey", "Robert", "Ryan",
-                "Vanessa", "Connor", "Tucker", "Annie", "Karley", "Kendra", "Samantha", "Kayla"};
-
-        String outdoorLocations[] = {"Upper Playing Fields", "Football field",
-                "Porter Park", "Lower Playing Fields"};
-
-        String indoorLocations[] = {"I-Center Courts", "Hart Gym"};
 
         int date = 0;
         int numPlayers = 0;
 
+        // Random object used to randomly pick configurations
         Random rand = new Random();
         int low = 0;
         int high = 0;
+
         for (int i = 0; i < numGames; i++) {
             listItem = "";
-
             // Generate a random game
             low = 0;
             high = 13;
@@ -95,23 +105,49 @@ public class PopulateListTask extends AsyncTask<Void, String, Void> {
             high = 16;
             numPlayers = rand.nextInt(high-low) + low;
 
+            // Create the string that will be displayed in the list view
             listItem = game + " - " + hour + ":00PM, " + day + " " + date + " - Players: " + numPlayers;
             //listItem.format("%s -- %d:00PM, %s %d -- Players: %d", game, hour, day, date, numPlayers);
             publishProgress(listItem);
             Log.d("PopulateListTask", listItem);
+
             Game newGame = new Game();
             newGame.setComments("Come for a great game!");
             newGame.setDate(day + " " + date);
 
+            // Generate a location
             if (game == "Football" || game == "Rugby" || game == "Softball" || game == "Soccer"
                     || game == "Spikeball" || game == "Ultimate Frisbee") {
                 low = 0;
                 high = 4;
                 newGame.setLocation(outdoorLocations[rand.nextInt(high - low)]);
             }
-            // put all cases here for locations else if (game == "Basketball" || "Badminton" || )
+            else if (game == "Basketball" || game == "Badminton" || game == "Hockey" || game == "Volley Ball") {
+                low = 0;
+                high = 2;
+                newGame.setLocation(indoorLocations[rand.nextInt(high - low)]);
+            }
+            else if (game == "Racquetball" || game == "Walley Ball") {
+                newGame.setLocation("Racquetball Courts");
+            }
+            else if (game == "Golf") {
+                newGame.setLocation("Golf Course");
+            }
+            else {
+                newGame.setLocation("TBD");
+            }
 
+            // Generate a list of players
+            for (int j = 0; j < numPlayers; j++) {
+                List<String> playerList = new ArrayList<>();
 
+                low = 0;
+                high = 16;
+                playerList.add(players[rand.nextInt(high - low)]);
+                newGame.setPlayers(playerList);
+            }
+
+            _games.add(newGame);
         }
 
         return null;
