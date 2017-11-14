153dce28935445cf22794aa7cfd23ddfc00d99a3
Daniel Hornberger <hor12020@byui.edu>
2017-11-13 20:39:59 -0700

    Modified DatePickerFragment so it works

--- app/src/main/java/com/pickup/daniel/pick_up/DatePickerFragment.java
+++ app/src/main/java/com/pickup/daniel/pick_up/DatePickerFragment.java
@@ -6,8 +6,42 @@ import android.app.DialogFragment;
 import android.os.Bundle;
 import android.widget.DatePicker;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class DatePickerFragment extends DialogFragment
         implements DatePickerDialog.OnDateSetListener {
+
+    // https://stackoverflow.com/questions/24558835/how-can-i-pass-the-date-chosen-in-a-date-picker-to-the-activity-which-contains-t
+
+    // Member variable to hold the reference to the listener
+    private DatePickerFragmentListener datePickerListener;
+
+    // Nested interface used to pass the date back to the activity
+    public interface DatePickerFragmentListener {
+        public void onDateSet(Date date);
+    }
+
+    public DatePickerFragmentListener getDatePickerListener() {
+        return this.datePickerListener;
+    }
+
+    public void setDatePickerListener(DatePickerFragmentListener listener) {
+        this.datePickerListener = listener;
+    }
+
+    protected void notifyDatePickerListener(Date date) {
+        if(this.datePickerListener != null) {
+            this.datePickerListener.onDateSet(date);
+        }
+    }
+
+    // Factory method to create new instances of the date picker with the listeners set up correctly
+    public static DatePickerFragment newInstance(DatePickerFragmentListener listener) {
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setDatePickerListener(listener);
+        return fragment;
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -25,6 +59,11 @@ public class DatePickerFragment extends DialogFragment
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
-        // Do something with the date chosen by the user
+        Calendar c = Calendar.getInstance();
+        c.set(year, month, day);
+        Date date = c.getTime();
+
+        // Here we call the listener and pass the date back to it.
+        notifyDatePickerListener(date);
     }
 }
