13d9f5b3639cf80de19131c04f8fa78fbd362e02
Daniel Hornberger <hor12020@byui.edu>
2017-11-15 09:18:03 -0700

    Added onJoin to bring up a text input dialog to get the user's name

--- app/src/main/java/com/pickup/daniel/pick_up/DisplayDetailsActivity.java
+++ app/src/main/java/com/pickup/daniel/pick_up/DisplayDetailsActivity.java
@@ -1,10 +1,14 @@
 package com.pickup.daniel.pick_up;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -49,5 +53,35 @@ public class DisplayDetailsActivity extends AppCompatActivity {
                 playersBlock.append(players.get(i));
             }
         }
+    }
+
+    String m_Text;
+
+    public void onJoin(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter your name to join!");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);// | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
